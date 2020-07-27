378c5c1063c68dad7995c4c260408208885858d0
ketakichalse19 <ketakichalse24@gmail.com>
2020-07-27 16:44:42 +0000

    TestBase by ketaki

--- PageOjectModelOfflineWebsite/src/test/java/com/test/TestBase.java
+++ PageOjectModelOfflineWebsite/src/test/java/com/test/TestBase.java
@@ -1,35 +1,43 @@
 package com.test;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 public class TestBase {
-	
-    public static WebDriver driver;
+
+	public static WebDriver driver;
 	Properties prop = null;
 	FileInputStream fis = null;
-	
-	@BeforeSuite
-	public void launchApplication() throws Throwable{
-		
-	 fis = new FileInputStream(System.getProperty("user.dir")+ "/src/main/resources/config.properties");
-	 prop = new Properties();
-	 prop.load(fis);
-	 
-     String URL = prop.getProperty("url");     
-     System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
-     driver = new ChromeDriver();
-     driver.get(URL);
-     driver.manage().window().maximize();
+
+	String readAnyProperty(String propFileName, String propName) {
+		String val = null;
+		try {
+			fis = new FileInputStream(System.getProperty("user.dir") + "/src/main/resources/" + "/config.properties");
+			prop = new Properties();
+			prop.load(fis);
+
+			val = prop.getProperty(propName);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return val;
 	}
-	
-	@AfterSuite
+
+	public void launchApplication() throws Throwable {
+		String URL = readAnyProperty("config.properties", "url");
+		System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get(URL);
+		driver.manage().window().maximize();
+	}
+
 	public void CloseLaunchApplication() {
 		driver.close();
 	}
 }
-
