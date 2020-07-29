3a42829d4d59a6b4941a2a0666103812cf717ad0
ashwinid111 <ashwinid111@gmail.com>
2020-07-29 13:12:17 +0530

    downloadFile

---
+++ PageOjectModelOfflineWebsite/src/main/java/com/pages/DownloadPage.java
@@ -0,0 +1,238 @@
+package com.pages;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+
+public class DownloadPage {
+	WebDriver driver = null;
+	DownloadPage dp = null;
+	ExtentTest test1 = null;
+
+	public DownloadPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+
+	Logger logger = Logger.getLogger(this.getClass());
+
+	@FindBy(id = "email")
+	public WebElement username;
+
+	@FindBy(id = "password")
+	public WebElement password;
+
+	@FindBy(xpath = "//button")
+	public WebElement loginButton;
+
+	public void typeUserName(String uname1) {
+		username.sendKeys(uname1);
+	}
+
+	public void typePassword(String pass) {
+		password.sendKeys(pass);
+	}
+
+	@FindBy(xpath = "//span[text()='Downloads']")
+	public WebElement downloads;
+
+	@FindBy(xpath = "//tr//th")
+	private List<WebElement> tableColumn;
+
+	@FindBy(xpath = "//tr//td")
+	private List<WebElement> tableRow;
+
+	@FindBy(xpath = "//tr[1]")
+	private WebElement header;
+
+	@FindBy(xpath = "//tr[4]//td[3]//following::tr[1]//td[3]")
+	private WebElement tableCell;
+
+	@FindBy(xpath = "//span[text()='32bit']")
+	private WebElement bit32Link;
+
+	@FindBy(xpath = "//span[text()='32bit']")
+	private WebElement bit64Link;
+
+	@FindBy(xpath = "//span[text()='32bit']")
+	private WebElement officialWebsiteLink;
+
+	@FindBy(xpath = "//table[@class='table table-hover']//child::tr//td[3]")
+	private List<WebElement> tableVendorsList;
+
+	@FindBy(xpath = "//table[@class='table table-hover']//child::tr//td[1]")
+	private List<WebElement> tableSrNumbers;
+
+	public DownloadPage navigateToDownloadPage() {
+		typeUserName("kiran@gmail.com");
+		typePassword("123456");
+		loginButton.click();
+		downloads.click();
+		return new DownloadPage(driver);
+	}
+
+	public boolean checkTotalRowColNum(ExtentTest logger2) {
+
+		logger2.log(Status.INFO, "Finding number of rows and columns:");
+		List<WebElement> listOfCol = tableColumn;
+		logger2.log(Status.INFO, "num of column" + listOfCol.size());
+		List<WebElement> listOfRows = tableRow;
+		logger2.log(Status.INFO, "num of rows" + listOfRows.size());
+		if (listOfCol.size() != listOfRows.size()) {
+			logger.info("The size of rows and coloumns not same.");
+			return true;
+		}
+		return false;
+	}
+
+	public boolean validateHeaderCount(ExtentTest logger2) {
+		// test1.log(Status.PASS, "asd");
+		logger2.log(Status.INFO, "Finding list of headers");
+		List<WebElement> listOfHeader = header.findElements(By.tagName("th"));
+		logger2.log(Status.INFO, "Header list finding completed ");
+		logger2.log(Status.INFO, "Total no of headers::" + listOfHeader.size());
+
+		if (listOfHeader.size() == 8) {
+			logger2.log(Status.PASS, "Header size is matched");
+			return true;
+		} else {
+			logger2.log(Status.FAIL, "Header size is not matched");
+			return false;
+		}
+	}
+
+	public boolean checkFollowVender(ExtentTest logger2) {
+		logger2.log(Status.INFO, "Checking Following Vendor of Selenium ");
+		String actVendor = tableCell.getText();
+		logger2.log(Status.INFO, "Following Vendor of Selenium " + actVendor);
+		String expVendor = "Google Chrome";
+		logger2.log(Status.INFO, "Expected Following Vendor of Selenium is" + expVendor);
+		if (actVendor.equals(expVendor)) {
+			logger2.log(Status.PASS, "Source Vendor match with expected vendor.." + actVendor);
+			return true;
+		} else {
+			logger2.log(Status.FAIL, "Source Vendor not match with expected vendor.." + actVendor);
+			return false;
+		}
+
+	}
+
+	public boolean checkLink32bitClickable(ExtentTest logger2) {
+		logger2.log(Status.INFO, "Validate 32bit link click or not");
+		try {
+			bit32Link.click();
+			logger2.log(Status.PASS, "32bit link is clickable");
+			return true;
+		} catch (Throwable t) {
+			logger2.log(Status.FAIL, "32bit link is not clickable");
+			return false;
+		}
+	}
+
+	public boolean checkLink64bitClickable(ExtentTest logger2) {
+		logger2.log(Status.INFO, "Validate 64bit link click or not");
+		try {
+			bit64Link.click();
+			logger2.log(Status.PASS, "64bit link is clickable");
+			return true;
+		} catch (Throwable t) {
+			logger2.log(Status.FAIL, "64Bit link not click suceefully..");
+			return false;
+		}
+	}
+
+	public boolean checkOfficialWebsiteClickable(ExtentTest logger2) {
+		logger2.log(Status.INFO, "Official Website link click()...");
+		try {
+			officialWebsiteLink.click();
+			logger2.log(Status.PASS, "Official Website link click..");
+			return true;
+		} catch (Throwable t) {
+			logger2.log(Status.FAIL, "Official Website link not click suceefully..");
+			return false;
+		}
+	}
+
+	public boolean checkVendorListIsSort(ExtentTest logger2) {
+		logger2.log(Status.INFO, "Validate Is vendor name list sorted");
+		ArrayList<String> actualWebList = new ArrayList<>();
+		List<WebElement> elementList = tableVendorsList;
+		logger2.log(Status.INFO, "Actul list of Vendor Sequence..");
+		for (WebElement we : elementList) {
+			actualWebList.add(we.getText());
+		}
+		logger2.log(Status.INFO, "Actul list of Vendor Name.." + actualWebList);
+		ArrayList<String> sortedList = new ArrayList<>();
+		for (String s : actualWebList) {
+			sortedList.add(s);
+		}
+		Collections.sort(sortedList);
+		logger2.log(Status.INFO, "Sorted actul list of Vendor Name..");
+		if (sortedList.equals(actualWebList)) {
+			logger2.log(Status.PASS, "Vendor List is sorted.." + sortedList);
+			return true;
+		} else {
+			logger2.log(Status.FAIL, "Vendor unsorted List after sorting.." + sortedList);
+			logger.debug(sortedList);
+			return false;
+		}
+	}
+
+	public boolean checkSrNumberListInSort(ExtentTest logger2) {
+		logger2.log(Status.INFO, "Validate Is Sr. numbers list sorted");
+		ArrayList<String> actualSrNumList = new ArrayList<>();
+		List<WebElement> elementList = tableSrNumbers;
+		logger2.log(Status.INFO, "Actul list of Sr. numbers Sequence..");
+		for (WebElement we : elementList) {
+			actualSrNumList.add(we.getText());
+		}
+		logger.debug(actualSrNumList);
+		logger2.log(Status.INFO, "Actul list of Sr. numbers.." + actualSrNumList);
+		ArrayList<String> sortedList = new ArrayList<>();
+		for (String s : actualSrNumList) {
+			sortedList.add(s);
+		}
+		Collections.sort(sortedList);
+		logger2.log(Status.INFO, "Sorted actul list of Vendor Name..");
+		if (sortedList.equals(actualSrNumList)) {
+			logger2.log(Status.PASS, "List is sorted" + sortedList);
+			return true;
+		} else {
+			logger2.log(Status.FAIL, "List is not sorted" + sortedList);
+			return false;
+		}
+	}
+
+	public boolean checkVendorStartWith(ExtentTest logger2) {
+		logger2.log(Status.INFO, "Validate Is vendor name start with Google");
+		ArrayList<String> actualVendorList = new ArrayList<>();
+		List<WebElement> elementList = tableVendorsList;
+		logger2.log(Status.INFO, "Actul list of Vendors..");
+		for (WebElement we : elementList) {
+			actualVendorList.add(we.getText());
+		}
+		logger.debug(actualVendorList);
+		logger2.log(Status.INFO, "Actul list of Sr. numbers.." + actualVendorList);
+		ArrayList<String> matchList = new ArrayList<>();
+		boolean status = true;
+
+		for (String vendor : actualVendorList) {
+			status = vendor.startsWith("Google");
+			logger.debug(status);
+			if (status == true) {
+				logger2.log(Status.PASS, " Vendor name Starts with Google..");
+				matchList.add("status");
+				logger2.log(Status.INFO, "Size of vendors list." + matchList.size());
+			}
+		}
+		return true;
+	}
+}
---
+++ PageOjectModelOfflineWebsite/src/test/java/com/test/DownloadTest.java
@@ -0,0 +1,119 @@
+package com.test;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import com.aventstack.extentreports.Status;
+import com.pages.DownloadPage;
+
+public class DownloadTest extends TestBase {
+
+	WebDriver driver = null;
+	DownloadPage dp = null;
+	DownloadPage dwp=null;
+
+	// Extent Report Variables
+	String pageName = "DOWNLOAD PAGE TESTCASES";
+	String extentReprtName = "DownloadPageExtentReport";
+	String hostName = "Offline Website";
+	String environment = "Download Page Testing";
+	String uName = "AshwiniD";
+	String docTitle = "DownloadPage";
+	String reportName = "DownloadPageExtentReport ";
+
+	// ExtentReport Methods
+	@BeforeTest()
+	public void beforeTest() {
+		super.setReport(extentReprtName, hostName, environment, uName, docTitle, reportName);
+	}
+
+	@AfterTest
+	public void afterTest() {
+		super.endReport();
+	}
+
+	@BeforeMethod
+	public void loadUrl() throws Throwable {
+	String URL = readAnyProperty("config.properties", "url");
+		System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get(URL);
+		driver.manage().window().maximize();
+		//super.launchApplication();
+		dp = new DownloadPage(driver);
+		dwp = dp.navigateToDownloadPage();
+	}
+
+	@AfterMethod
+	public void closeBrowser() {
+		testlogger.log(Status.INFO, "Browser Closed");
+		driver.close();
+	}
+
+	@Test(priority = 1)
+	public void validateCounOfHeaderColumns() throws Exception {
+		super.passTest("validateCounOfHeaderColumns", pageName);// ExtentReport
+																// Test Create
+		Assert.assertTrue(dp.validateHeaderCount(testlogger));
+		Thread.sleep(3000);
+	}
+
+	/*@Test(priority = 2)
+	public void validateNoOfRowColumn() throws Exception {
+		super.passTest("validateNoOfRowColumn", pageName);
+		Assert.assertTrue(dp.checkTotalRowColNum(testlogger));
+		Thread.sleep(3000);
+	}
+
+	@Test(priority = 3)
+	public void validateFollowVender() throws Exception {
+		super.passTest("validateFollowVender", pageName);
+		Assert.assertTrue(dp.checkFollowVender(testlogger));
+		Thread.sleep(3000);
+	}
+
+	@Test(priority = 4)
+	public void check32bitLink() throws Exception {
+		super.passTest("check32bitLink", pageName);
+		Assert.assertTrue(dp.checkLink32bitClickable(testlogger));
+		Thread.sleep(3000);
+	}
+
+	@Test(priority = 5)
+	public void check64bitLink() throws Exception {
+		super.passTest("check64bitLink", pageName);
+		Assert.assertTrue(dp.checkLink64bitClickable(testlogger));
+		Thread.sleep(3000);
+	}
+
+	@Test(priority = 6)
+	public void checkOfficialWebsite() {
+		super.passTest("CheckOfficialWebsite", pageName);
+		Assert.assertTrue(dp.checkOfficialWebsiteClickable(testlogger));
+	}
+
+	@Test(priority = 8)
+	public void validateVendorsNumberListSequence() {
+		super.passTest("ValidateVendorsNumberListSequence", pageName);
+		Assert.assertTrue(dp.checkVendorListIsSort(testlogger));
+	}
+
+	@Test(priority = 9)
+	public void validateSrNumberListSequence() {
+		super.passTest("ValidateSrNumberListSequence", pageName);
+		Assert.assertTrue(dp.checkSrNumberListInSort(testlogger));
+	}
+
+	@Test(priority = 10)
+	public void validateStartWithVendorName() {
+		super.passTest("ValidateStartWithVendorName", pageName);
+		Assert.assertTrue(dp.checkVendorStartWith(testlogger));
+	}
+*/
+}
